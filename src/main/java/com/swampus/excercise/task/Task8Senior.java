2c56778edb887ab8cc617fd893299ba23f1bd752
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-08-15 10:04:23 +0100

    fix

--- src/main/java/com/swampus/excercise/task/Task8Senior.java
+++ src/main/java/com/swampus/excercise/task/Task8Senior.java
@@ -3,7 +3,7 @@ package com.swampus.excercise.task;
 import com.swampus.excercise.not.modify.Task8Extender;
 
 /**
- * USE INCAPSULATION
+ * USE POLYMORPHISM
  * method fly should return 'fly'
  */
 public class Task8Senior extends Task8Extender {
